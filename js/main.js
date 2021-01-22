58322b189049db278a3dc4049741cff1f02d66e7
Vakhitov-Aleksandr <ale.vakhitov@gmail.com>
2021-01-22 22:20:37 +0500

    featured

--- js/main.js
+++ js/main.js
@@ -5,12 +5,19 @@ function menuToggle() {
   menuTopBurger.classList.toggle('active');
   let menuTop = document.querySelector('.menu-top');
   menuTop.classList.toggle('open');
+  
   let menuTopLinks = document.querySelectorAll('.menu-top__link');
   for (let index = 0; index < menuTopLinks.length; index++) {
     const menuTopLink = menuTopLinks[index];
     menuTopLink.addEventListener('click', function () {
       menuTop.classList.remove('open');
       menuTopBurger.classList.remove('active');
+      document.body.style.overflow = '';
     });
+  }
+  if(menuTop.classList.contains('open')){
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
   }
 }
